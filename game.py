3e70cdaf6ec231e25912f7738ee573ac11dccfc3
CallumDennis <callumdennis45@hotmail.com>
2016-10-18 03:59:10 +0100

    Add files via upload
    Added the execute_buy/sell to the execute_command as well as them adding them into the game.py file

--- game.py
+++ game.py
@@ -55,7 +55,7 @@ def is_valid_exit(exits, chosen_exit):
     return chosen_exit in exits
 
 
-# updates the current room based on the direction (example execut_go("east"))
+# updates the current room based on the direction (example execute_go("east"))
 def execute_go(direction):
     global current_room
     if direction in current_room["exits"].keys():
@@ -66,6 +66,60 @@ def execute_go(direction):
         current_room["description"] = descriptions[1]
     else:
         print("You cannot go there.")
+
+def execute_drop(item_id):
+    for item in inventory:
+        if item_id == item["id"]:
+            inventory.remove(item)
+            current_room["items"].append(item)
+            print("you have dropped " + str(item["name"]))
+            break
+        else:
+            print("You cannot drop that")
+
+def execute_take(item_id):
+    for item in current_room["items"]:
+        if item_id == item["id"]:
+            inventory.append(item)
+            current_room["items"].remove(item)
+            print("you have taken " + str(item["name"]))
+            break
+        else:
+            print("You cannot take that.")
+
+def execute_buy(item_id):
+    global credit
+    # credit is made global as the value is being changed
+    for item in current_room["stall"]:
+        # searches the room for a stall dictionary
+        if item_id == item["id"]:
+            if credit > item["cost"]:
+                # checks you have enough money
+                inventory.append(item)
+                current_room["stall"].remove(item)
+                credit = credit - item["cost"]
+                # adds the item to your inv and then changes your credits
+                print("you have bought " + str(item["name"]))
+                print("you have " + str(credit) + " credits")
+                break
+            else:
+                print("You do not have enough credits")
+
+def execute_sell(item_id):
+    global credit
+
+    for item in inventory:
+        if item_id == item["id"]:
+            inventory.remove(item)
+            credit = credit + int((0.5 * int(item["cost"])))
+            current_room["stall"].append(item)
+            print("you have sold " + str(item["name"]) + " you have gained, " + str(
+            int(0.5 * item["cost"])) + " credits.")
+            print("you have " + str(credit) + " credits")
+            # same thing as buy but the other way around, but only gives you half of the credits back
+            break
+        else:
+            print("You cannot sell that")
 
 
 #  prints the menu of actions using print_menu() function.
@@ -98,6 +152,27 @@ def execute_command(command):
             execute_go(command[1])
         else:
             print("Go where?")
+    elif command[0] == "take":
+        if len(command) > 1:
+            execute_take(command[1])
+        else:
+            print("Take what?")
+    elif command[0] == "drop":
+        if len(command) > 1:
+            execute_drop(command[1])
+        else:
+            print("Drop what?")
+    elif command[0] == "buy":
+        if len(command) > 1:
+            execute_buy(command[1])
+        else:
+            print("Buy what?")
+    elif command[0] == "sell":
+        if len(command) > 1:
+            execute_sell(command[1])
+        else:
+            print("Sell what?")
+
 
 
 def main():
