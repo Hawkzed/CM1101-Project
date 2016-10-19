a5914c9178290073eb3aaaf210a3b4a61270a0e5
jacquesmill <millard64@cock.li>
2016-10-19 20:09:14 +0100

    Changed stat changes
    Made it so game checks inventory for stat change items whenever the
    player moves, removes the item, and makes the accordant stat changes.

--- player.py
+++ player.py
@@ -3,15 +3,3 @@ from map import rooms
 current_room = rooms["Room 2"]
 player_stats = {"strength": 5,"defence": 5, "health": 100, "weapon":10, "credits": 50}
 inventory = []
-
-for key,value in inventory:
-	if value["id"] == "1":
-		player_stats["strength"] += 5
-	elif value["id"] == "2":
-		player_stats["defence"] += 5
-	elif value["id"] == "3":
-		player_stats["health"] += 25
-	elif value["id"] == "4":
-		player_stats["strength"] += 5
-		player_stats["defence"] += 5
-		player_stats["health"] += 50
